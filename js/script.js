ce69620d4c770d0a98e5ff5bdcf4767a3e4e2ad0
Jack Bogart <jackbogart0822@gmail.com>
2024-02-25 02:17:11 -0500

    Add the ability to resize the grid.
    Users will be prompted upon selecting the resize button to input a size
    of the grid up to 100x100.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,50 @@
 const grid = document.querySelector('#grid');
 
-function initialization() {
-    const tileStyle = Math.floor(700 / 16);
-    for (let i = 0; i < 16 ** 2; i++) {
+function createGrid(size = 16) {
+    for (let i = 0; i < size ** 2; i++) {
         const tile = document.createElement('div');
         tile.classList.add('tile');
-        tile.setAttribute('style', `height:${tileStyle - 2}px; width:${tileStyle - 2}px;`);
+        tile.style.width = `${100 / size}%`;
+        tile.style.height = `${100 / size}%`;
         tile.addEventListener('mouseenter', (event) => {
             event.target.style.backgroundColor = 'white';
         });
         grid.appendChild(tile);
     }
+}
+
+function deleteGrid() {
+    const tileList = document.querySelectorAll('.tile');
+
+    tileList.forEach((tile) => {
+        tile.remove();
+    });
+}
+
+function changeSize() {
+    let newGridSize;
+    do {
+        newGridSize = prompt('Select new grid size (max 100x100): ');
+    } while (newGridSize > 100 || newGridSize < 1);
+    deleteGrid();
+    createGrid(newGridSize);
+}
+
+function resetGrid() {
+    const tileList = document.querySelectorAll('.tile');
+
+    console.log('reset');
+    tileList.forEach((tile) => {
+        tile.style.backgroundColor = '';
+    });
+}
+
+function initialization() {
+    const size_btn = document.querySelector('#size_btn');
+    size_btn.addEventListener('click', changeSize);
+    const reset_btn = document.querySelector('#reset_btn');
+    reset_btn.addEventListener('click', resetGrid);
+    createGrid();
 }
 
 initialization();
